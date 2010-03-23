1e4c52b919a6720c83729212e56ccfc6786bd9ea
Andrew Cantino <cantino@gmail.com>
2010-03-22 22:36:54 -0700

    added code and license

---
+++ jasmine-node.js
@@ -0,0 +1,152 @@
+// By Andrew Cantino (Iteration Labs, LLC) and David Stevenson
+
+var fs = require("fs"),
+    path = require("path"),
+    sys = require("sys");
+
+var jasminePath = path.join(path.dirname(__filename), "..", "..", "lib", "jasmine-0.10.1.js");
+sys.puts("Loading jasmine from " + jasminePath);
+
+var window = GLOBAL;
+eval(fs.readFileSync(jasminePath));
+
+ansiColor = function(color) {
+  var colors = {
+    green: "32",
+    red: "31",
+    yellow: "33"
+  };
+
+  if (color) {
+    sys.print("\u001B[" + colors[color] + "m")
+  } else {
+    sys.print("\u001B[0m")
+  }
+};
+
+jasmine.NodeReporter = function() {
+  this.failedCount = 0;
+  this.successCount = 0;
+  this.failures = [];
+  this.startTime = (new Date()).getTime();
+};
+
+//noinspection JSUnusedLocalSymbols
+jasmine.NodeReporter.prototype.reportRunnerStarting = function(runner) {
+  sys.puts("Starting tests...");
+};
+
+//noinspection JSUnusedLocalSymbols
+jasmine.NodeReporter.prototype.reportRunnerResults = function(runner) {
+  sys.puts("\n")
+  for (var i = 0; i < this.failures.length; i++) {
+    var failure = this.failures[i];
+    sys.puts("Failing test in '" + failure.getFullName() + "'")
+    var items = failure.results().getItems();
+    for (var j = 0; j < items.length; j++) {
+      var item = items[j];
+      if (item.passed())
+        continue;
+      sys.puts(item.trace.stack);
+    }
+    sys.puts("")
+  }
+  var endTime = (new Date()).getTime();
+
+  sys.puts("Tests finished in " + (endTime - this.startTime) + "ms. " + this.successCount + " success, " + this.failedCount + " failed.");
+};
+
+//noinspection JSUnusedLocalSymbols
+jasmine.NodeReporter.prototype.reportSuiteResults = function(suite) {
+};
+
+//noinspection JSUnusedLocalSymbols
+jasmine.NodeReporter.prototype.reportSpecResults = function(spec) {
+  var result = spec.results();
+  if (result.passed()) {
+    this.successCount++;
+    ansiColor("green");
+    sys.print(".");
+  } else if (result.skipped) {
+    ansiColor("yellow");
+    sys.print("*");
+  } else {
+    ansiColor("red");
+    this.failedCount++;
+    this.failures.push(spec);
+    sys.print("F");
+  }
+  ansiColor(null);
+};
+
+//noinspection JSUnusedLocalSymbols
+jasmine.NodeReporter.prototype.log = function(str) {
+};
+
+for (var a in jasmine) {
+  if (jasmine.hasOwnProperty(a)) exports[a] = jasmine[a];
+}
+
+exports.include = function(url, opt_global) {
+//  var jasmineKeys = [];
+//  var jasmineValues = []
+//  if(!opt_global)
+//    opt_global = GLOBAL;
+//  for(k in opt_global) {
+//    jasmineKeys.push(k);
+//    jasmineValues.push(jasmine[k]);
+//  }
+//  fileData =  fs.readFileSync(url);
+//  fileData = "function testotron(" + jasmineKeys.join(",") + ") { "+fileData+" }";
+//  sys.puts(fileData)
+//  process.compile(fileData, url);
+//  testotron.apply(GLOBAL, [jasmineValues]);
+//  eval(fs.readFileSync(url));
+  var data = fs.readFileSync(url);
+//  sys.puts(data);
+  eval(data);
+//  process.evalcx(data);
+//  process.compileUnsafe(data, url);
+};
+
+filesFromGlob = function(path) {
+  var glob = path.replace(/\./g, "\\.").replace(/\*/g, "[^/]*").replace(/\[\^\/\]\*\[\^\/\]\*/g, ".*").replace(/\?/g, ".")
+  var regex = new RegExp(glob, "gi");
+  var rootDir = "."
+  var dirQueue = [rootDir];
+  var files = [];
+  while (dirQueue.length > 0) {
+    var dir = dirQueue.shift();
+    var dirContents = fs.readdirSync(dir);
+    for (var i = 0; i < dirContents.length; i++) {
+      var file = dirContents[i];
+      var relativePath = dir + "/" + file;
+      var stat = fs.lstatSync(relativePath);
+      if (stat.isDirectory()) {
+        dirQueue.push(relativePath);
+      } else if (stat.isFile()) {
+        if (regex.test(relativePath)) {
+          files.push(relativePath);
+        } else {
+        }
+      }
+    }
+  }
+  return files;
+};
+
+exports.includeAll = function(path, opt_global) {
+  var files = filesFromGlob(path);
+  for (var i = 0; i < files.length; i++) {
+    exports.include(files[i], opt_global);
+  }
+};
+
+exports.runDefaultNodeSpecs = function() {
+  exports.include("spec/spec_helper.js");
+  exports.includeAll("spec/*_spec.js");
+  exports.includeAll("spec/**/*_spec.js");
+  var jasmineEnv = jasmine.getEnv();
+  jasmineEnv.reporter = new jasmine.NodeReporter();
+  jasmineEnv.execute();
+};
